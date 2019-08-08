0b1c27c164299f643f60c95368bb7ebe4673298b
Lyndsey Reese <lmariereese@gmail.com>
2019-08-08 17:48:52 -0400

    Add page will redirect

--- routes/wiki.js
+++ routes/wiki.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../models');
 const addPage = require('../views/addPage');
 const { Page } = require("../models");
+const wikiPage = require("../views/wikipage");
 
 
 router.get('/', async (req, res, next) => {
@@ -10,18 +11,14 @@ router.get('/', async (req, res, next) => {
 });
 
 router.post('/', async (req, res, next) => {
-
-
   const page = new Page({
     title: req.body.title,
-    content: req.body.content,
-    status: req.body.status
+    content: req.body.content
 
   })
   try {
-    // await page.save();
-    console.log(page);
-    res.redirect('/');
+    await page.save();
+    res.redirect(`${page.slug}`);
   } catch (error) { next(error) }
 });
 
@@ -29,6 +26,12 @@ router.get('/add', async (req, res, next) => {
   res.send(addPage())
 });
 
-
+router.get('/:slug', async (req, res, next) => {
+  const foundPage = await Page.findOne({
+    where: {slug: req.params.slug}
+  });
+  res.send(wikiPage(foundPage));
+  // res.send(`hit dynamic route at ${req.params.slug}`);
+});
 
 module.exports = router;
